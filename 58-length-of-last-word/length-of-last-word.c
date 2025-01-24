96d7596627ccb768442a6c96610771698dd6f514
ANUPAMA K V <127279063+anupama3440@users.noreply.github.com>
2025-01-24 09:12:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (11.45%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.c
@@ -0,0 +1,12 @@
+int lengthOfLastWord(char *s) {
+    int length = 0;
+    int i = strlen(s) - 1;
+    while (i >= 0 && isspace(s[i])) {
+        i--;
+    }
+    while (i >= 0 && !isspace(s[i])) {
+        length++;
+        i--;
+    }
+    return length;
+}
