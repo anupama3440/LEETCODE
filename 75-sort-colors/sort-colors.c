4a187d36bad244d9db38bfe21f0234d16ef648cc
ANUPAMA K V <127279063+anupama3440@users.noreply.github.com>
2025-04-25 09:52:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (95.03%) - LeetSync

---
+++ 75-sort-colors/sort-colors.c
@@ -0,0 +1,19 @@
+void sortColors(int* nums, int numsSize) {
+    int count0 = 0, count1 = 0, count2 = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == 0) count0++;
+        else if (nums[i] == 1) count1++;
+        else count2++;
+    }
+
+    int index = 0;
+    for (int i = 0; i < count0; i++) {
+        nums[index++] = 0;
+    }
+    for (int i = 0; i < count1; i++) {
+        nums[index++] = 1;
+    }
+    for (int i = 0; i < count2; i++) {
+        nums[index++] = 2;
+    }
+}
